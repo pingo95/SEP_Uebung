e86749733a09fbc0dcf556decdd931444a72ba09
RTB <robin.tim.broeske@rwth-aachen.de>
2015-06-26 01:08:06 +0200

    translator unabhängig von der eig system sprache

--- Quellcode/main.cpp
+++ Quellcode/main.cpp
@@ -1,14 +1,14 @@
 #include "../Header-Dateien/mainwindow.h"
 #include <QApplication>
 
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
     QTranslator translator;
-    translator.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    translator.load("qt_de_DE", QLibraryInfo::location(QLibraryInfo::TranslationsPath));
     app.installTranslator(&translator);
-
     MainWindow w;
     w.show();
 
